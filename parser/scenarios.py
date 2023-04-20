bbb1059e166db9bc8b3f324494d12225225e763c
gudyfr <106124392+gudyfr@users.noreply.github.com>
2023-04-19 23:06:03 -0700

    Layout parsing progress

--- parser/scenarios.py
+++ parser/scenarios.py
@@ -24,7 +24,7 @@ def positionToJson(pt,w,h,score, xOffset, yOffset):
         "score" : score
     }
 
-def identify(out, img, templateFile, xOffset, yOffset, threshold=0.96):
+def identify(out, img, templateFile, xOffset, yOffset, threshold=0.96, showMatch=True):
     template = cv.imread(templateFile, flags=cv.IMREAD_UNCHANGED)
     assert template is not None, "template file could not be read, check with os.path.exists()"
     _, _, _, a_channel = cv.split(template)
@@ -51,7 +51,8 @@ def identify(out, img, templateFile, xOffset, yOffset, threshold=0.96):
         score = res[pt[1]][pt[0]]
         print("\t\tFound {} at ({}, {}) {}".format(
             templateFile, pt[0], pt[1], score))
-        cv.rectangle(out, (pt[0] + xOffset, pt[1]+yOffset), (pt[0] + w + xOffset, pt[1] + h+yOffset), (0, 0, 255), 1)
+        if showMatch:
+            cv.rectangle(out, (pt[0] + xOffset, pt[1]+yOffset), (pt[0] + w + xOffset, pt[1] + h+yOffset), (0, 0, 255), 1)
         output.append(positionToJson(pt,w,h,score,xOffset,yOffset))
 
     return output
@@ -90,27 +91,42 @@ scenarios = loadScenarios()
 tileInfos = loadTiles()
 scenarioIds = []
 args = sys.argv[1:]
+parseLayouts = False
+parseMaps = False
+if "-layout" in args:
+    args.remove('-layout')
+    parseLayouts = True
+if "-map" in args:
+    args.remove('-map')
+    parseMaps = True
 if "-all" in args:
     scenarioIds = scenarios.keys()
 else:
     for arg in args:
         scenarioIds.append(arg)
 
+def getScore(elem):
+    return elem["score"]
+
 for id in scenarioIds:
     nb = id
     scenario = scenarios[id]
-    results = []
+    print(f"{bcolors.HEADER}Analyzing Scenario #{id} : {scenario['title']}{bcolors.ENDC}")
+    scenarioData = {"id" : id, "maps" : []}    
     pagesToCover = []
     pagesToCover.append({"type" : "scenario", "page" : scenario["page"]})
     if 'otherPages' in scenario:
         for otherPage in scenario['otherPages']:
             pagesToCover.append({"type" : "scenario", "page" : otherPage})
-    if 'sections' in scenario:
+
+    # No need to parse Sections when we're not parsing maps
+    if 'sections' in scenario and parseMaps:
         for section in scenario['sections']:
             pagesToCover.append({"type" : "section", "page" : int(float(section))})
     
     layoutFound = False
     for page in pagesToCover:
+        results = []
         path = "scenarios/p" if page["type"] == "scenario"  else "sections/"
         imgFile = os.path.join(f"assets/pages/{path}{page['page']}.png")
         if os.path.exists(imgFile):        
@@ -121,55 +137,64 @@ for id in scenarioIds:
             img_gray = cv.cvtColor(img, cv.COLOR_RGBA2GRAY)
 
             pageHasLayout = False
-            layoutPosition = []
-            # See if this page has a layout definition
-            if not layoutFound:
-                mapLayout = 'assets/tiles/layout/Map Layout.png'
-                result = identify(out, img, mapLayout, 0, 0, 0.90)
-                if len(result) > 0:
-                    pageHasLayout = True
-                    layoutPosition = result[0]["position"]
-
+            layoutPosition = {}
+            layoutEnd = {}
             w = img.shape[1]
             h = img.shape[0]
 
+            # See if this page has a layout definition
+            if not layoutFound and parseLayouts:
+                result = identify(out, img, 'assets/tiles/layout/Map Layout.png', 0, 0, 0.90, False)
+                if len(result) > 0:
+                    pageHasLayout = True
+                    layoutPosition = result[0]["position"]
+                    layoutEndImg = img[layoutPosition["y"]:h, layoutPosition["x"]:w]
+                    result = identify(out, layoutEndImg, 'assets/tiles/layout/Map Layout End.png', layoutPosition["x"], layoutPosition["y"], 0.90, False)
+                    result.sort(key=getScore, reverse=True)
+                    if len(result) > 0:
+                        position = result[0]["position"]
+                        size = result[0]["size"]
+                        layoutEnd["x"] = position["x"] + size["x"]
+                        layoutEnd["y"] = position["y"] + size["y"]
             tiles = []
             mapTiles = []
             mapImg = None
             mapMinX = 0
             mapMinY = 0
             if pageHasLayout:
-                mapMinX = max(0, layoutPosition["x"] - 50)
-                mapMinY = max(0, layoutPosition["y"] - 50)                            
-                mapMaxX = min(w, layoutPosition["x"] + 1000)
-                mapMaxY = min(h, layoutPosition["y"] + 1000)    
+                if not "x" in layoutEnd:
+                    layoutEnd["x"] = min(w, layoutPosition["x"] + 1500)
+                    layoutEnd["y"] = min(h, layoutPosition["y"] + 1000)
+                mapMinX = layoutPosition["x"] - 50
+                mapMinY = layoutPosition["y"] - 50                           
+                mapMaxX = layoutEnd["x"]
+                mapMaxY = layoutEnd["y"]   
                 mapImg = img[mapMinY:mapMaxY, mapMinX:mapMaxX]
                 cv.rectangle(out, (mapMinX,mapMinY), (mapMaxX, mapMaxY), (255, 0, 0), 2)
 
-            if 'tiles' in scenario:
+            if 'tiles' in scenario and (parseMaps or parseLayouts):
                 for tile in scenario['tiles']:                    
                     found = False                    
-                    orientations = ["-0","-30", "-60","-90","-120","-180","-240","-270","-300"]                            
-                    for orientation in orientations:
-                        if pageHasLayout:
-                            mapTileFile = f'assets/tiles/layout/tiles/{tile}{orientation}.png'
-                            if os.path.exists(mapTileFile):
-                                result = identify(out, mapImg, mapTileFile, mapMinX, mapMinY, 0.92)
-                                if len(result) > 0:
-                                    mapTiles.append({"name" : tile, "orientation": orientation, "positions" : result})
+                    orientations = ["-0","-30", "-60","-90","-120","-180","-210","-240","-270","-300"]       
+                    variants = ["","-Alt"]                    
+                    for variant in variants:                     
+                        for orientation in orientations:
+                            if pageHasLayout and parseLayouts:
+                                mapTileFile = f'assets/tiles/layout/tiles/{tile}{orientation}{variant}.png'
+                                if os.path.exists(mapTileFile):
+                                    result = identify(out, mapImg, mapTileFile, mapMinX, mapMinY, 0.91)
+                                    if len(result) > 0:
+                                        mapTiles.append({"name" : tile, "orientation": orientation, "variant":variant, "positions" : result})
 
-                        variants = ["","-Alt"]                    
-                        for variant in variants:
-                            tileFile = f'assets/tiles/maps/{tile}{orientation}{variant}.png'
-                            if os.path.exists(tileFile):                            
-                                print(f"\tLooking for Tile {tile}{orientation}{variant}")     
-                                result = identify(out, img, tileFile, 0, 0, 0.90)
-                                if len(result) > 0:
-                                    found = True
-                                    results.append({"name" : tile, "variant": variant, "orientation": orientation, "type": "tile", "results" : result, "orientation" : orientation})
-                                    tiles.append({"name" : tile, "variant": variant, "orientation": orientation, "positions" : result})    
-                    if not found:
-                        print(f"{bcolors.WARNING}Couldn't find tile {tile}{bcolors.ENDC}")
+                        if parseMaps:                           
+                                tileFile = f'assets/tiles/maps/{tile}{orientation}{variant}.png'
+                                if os.path.exists(tileFile):                            
+                                    print(f"\tLooking for Tile {tile}{orientation}{variant}")     
+                                    result = identify(out, img, tileFile, 0, 0, 0.90)
+                                    if len(result) > 0:
+                                        found = True
+                                        results.append({"name" : tile, "variant": variant, "orientation": orientation, "type": "tile", "results" : result, "orientation" : orientation})
+                                        tiles.append({"name" : tile, "variant": variant, "orientation": orientation, "positions" : result})    
 
             w = img.shape[1]
             h = img.shape[0]
@@ -192,8 +217,11 @@ for id in scenarioIds:
                         minY = min(minY, max(y + tileInfo["minY"], 0))
                         maxY = max(maxY, min(y + tileInfo["maxY"], h))
 
-            if len(scenario["tiles"] if "tiles" in scenario else []) == len(mapTiles):
-                layoutFound = True
+            if parseLayouts:
+                if pageHasLayout:
+                    found = len(scenarioData["layout"]) if "layout" in scenarioData else 0
+                    if len(mapTiles) > found:
+                        scenarioData["layout"] = mapTiles                
 
             if len(tiles) > 0:
                 print(f"Lookup Area : ({minX},{minY}) -> ({maxX}, {maxY})")
@@ -242,5 +270,15 @@ for id in scenarioIds:
                             results.append({"name" : subEntry, "type": entry, "results" : result})
             t = "p" if page["type"] == "scenario" else "s"
             cv.imwrite(f"out/{nb}-{t}{page['page']}.png", out)
-            with open(f"out/{nb}-{t}{page['page']}.json", "w") as outfile:
-                json.dump({"scenario": id, "layout" : mapTiles, "type":page['type'], "page" : page['page'], "results":results}, outfile, indent=3, cls=NpEncoder)
+            scenarioData["maps"].append({"type":page['type'], "page" : page['page'], "results":results})
+
+    printColor = bcolors.WARNING
+    nbTotalTiles = len(scenario["tiles"] if "tiles" in scenario else [])
+    nbFoundTiles = len(scenarioData["layout"] if "layout" in scenarioData else [])
+    if nbTotalTiles == nbFoundTiles:
+        layoutFound = True
+        printColor = bcolors.OKGREEN
+    print(f"{printColor}Found {nbFoundTiles} out of {nbTotalTiles} layout tiles{bcolors.ENDC}")
+
+    with open(f"out/{nb}.json", "w") as outfile:
+        json.dump(scenarioData, outfile, indent=3, cls=NpEncoder)
